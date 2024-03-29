55115535d337bb0ac2ee889a0e46b07bde0639a8
Marco Rotiroti <marco.rotiroti@gmail.com>
2024-03-29 16:57:27 +0100

    feat: add sharpness, noise, contrast and BRISQUE metrics

--- fpctl/cli.py
+++ fpctl/cli.py
@@ -1,11 +1,12 @@
 import typer
 
-from . import dataset, feature, fingertip
+from . import dataset, feature, fingertip, quality
 
 app = typer.Typer(help="Fingerphoto Recognition Command Line Interface.")
 app.add_typer(fingertip.app, name="fingertip", help="Fingertip Commands")
 app.add_typer(feature.app, name="feature", help="Feature Commands")
 app.add_typer(dataset.app, name="dataset", help="Dataset Commands")
+app.add_typer(quality.app, name="quality", help="Image Quality Commands")
 
 if __name__ == "__main__":
     app()
---
+++ fpctl/quality.py
@@ -0,0 +1,54 @@
+import csv
+import shutil
+from pathlib import Path
+
+import cv2
+import numpy as np
+import typer
+from tqdm import tqdm
+
+from mobiofp.iqa import estimate_noise, laplacian_sharpness, rms_contrast
+
+app = typer.Typer()
+
+
+@app.command(help="Generate image quality assessment report.")
+def report(
+    source_directory: Path = typer.Argument(..., help="Path to the input images directory."),
+    target_directory: Path = typer.Argument(..., help="Path to the output directory."),
+    brisque_model: Path = typer.Option(
+        "./models/brisque_model_live.yml", help="Path to the BRISQUE model."
+    ),
+    brisque_range: Path = typer.Option(
+        "./models/brisque_range_live.yml", help="Path to the BRISQUE range."
+    ),
+    output_file: Path = typer.Option("iqa.csv", help="Path to the output report file."),
+):
+    report_dir = Path(target_directory)
+    report_dir.mkdir(parents=True, exist_ok=True)
+    report_file_path = report_dir / output_file
+    brisquer = cv2.quality.QualityBRISQUE_create(str(brisque_model), str(brisque_range))
+
+    with open(report_file_path, "w", newline="") as csvfile:
+        fieldnames = ["Image name", "Laplacian", "Noise", "Contrast", "BRISQUE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for image_path in tqdm(list(Path(source_directory).glob("*.jpg"))):
+            image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
+            laplacian = laplacian_sharpness(image)
+            noise = estimate_noise(image)
+            contrast = rms_contrast(image)
+            brisque = brisquer.compute(image)[0]
+            writer.writerow(
+                {
+                    "Image name": image_path.name,
+                    "Laplacian": laplacian,
+                    "Noise": noise,
+                    "Contrast": contrast,
+                    "BRISQUE": brisque,
+                }
+            )
+
+    typer.echo(f"Image quality assessment report saved to {report_file_path}")
+    typer.echo("Done.")

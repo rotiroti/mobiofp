67e06fc15178e71a06906f7e01b51c04c879faab
Marco Rotiroti <marco.rotiroti@gmail.com>
2024-03-30 11:05:52 +0100

    feat: create separate method for fingerprint thinning

---
+++ fpctl/fingerprint.py
@@ -0,0 +1,56 @@
+import cv2
+import typer
+
+from pathlib import Path
+from tqdm import tqdm
+from mobiofp.utils import fingerprint_mapping, fingerprint_enhancement
+
+app = typer.Typer()
+
+@app.command(help="Transform fingertip images into fingerprint images.")
+def convert(
+    source_directory: Path = typer.Argument(..., help="Path to the input images directory."),
+    target_directory: Path = typer.Argument(..., help="Path to the output directory."),
+):
+    output_dir = Path(target_directory) / "mapping"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for p in tqdm(list(Path(source_directory).glob("*.png"))):
+        image = cv2.imread(str(p), cv2.IMREAD_GRAYSCALE)
+        output = fingerprint_mapping(image)
+        output_path = output_dir / p.with_suffix(".png").name
+        cv2.imwrite(str(output_path), output)
+        typer.echo(f"Fingerprint enhanced image saved to {output_path}")
+    typer.echo("Done!")
+
+@app.command(help="Run fingerprint enhancement")
+def enhance(
+    source_directory: Path = typer.Argument(..., help="Path to the input images directory."),
+    target_directory: Path = typer.Argument(..., help="Path to the output directory."),
+):
+    output_dir = Path(target_directory) / "fingerprints"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for p in tqdm(list(Path(source_directory).glob("*.png"))):
+        image = cv2.imread(str(p), cv2.IMREAD_GRAYSCALE)
+        output = fingerprint_enhancement(image)
+        output_path = output_dir / p.with_suffix(".png").name
+        cv2.imwrite(str(output_path), output)
+        typer.echo(f"Fingerprint enhanced image saved to {output_path}")
+    typer.echo("Done!")    
+
+@app.command(help="Apply thinning algorithm to fingerprint images.")
+def thinning(
+    source_directory: Path = typer.Argument(..., help="Path to the input images directory."),
+    target_directory: Path = typer.Argument(..., help="Path to the output directory."),
+):
+    output_dir = Path(target_directory) / "thinning"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for p in tqdm(list(Path(source_directory).glob("*.png"))):
+        image = cv2.imread(str(p), cv2.IMREAD_GRAYSCALE)
+        output = cv2.ximgproc.thinning(image)
+        output_path = output_dir / p.with_suffix(".png").name
+        cv2.imwrite(str(output_path), output)
+        typer.echo(f"Thinning image saved to {output_path}")
+    typer.echo("Done!")
